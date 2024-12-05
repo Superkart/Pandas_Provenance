96e056f8020f9f433d289b6d6bfb97b0aaaeaf92
Superkart <karthik.ragi15@gmail.com>
2024-12-05 15:26:31 -0600

    Fixed code

--- Pandas_Provenance_Project/provenance/table_utils.py
+++ Pandas_Provenance_Project/provenance/table_utils.py
@@ -1,13 +1,9 @@
 import hashlib
 import pandas as pd
 
-
-def calculate_hash(df):
-    """Calculate a unique hash for a DataFrame based on its contents."""
-    hash_object = hashlib.sha256(pd.util.hash_pandas_object(df).values)
+def calculate_hash(dataframe):
+    hash_object = hashlib.sha256(pd.util.hash_pandas_object(dataframe).values)
     return hash_object.hexdigest()
 
-
 def generate_table_name(table_hash):
-    """Generate a table name based on its hash."""
     return f"table_{table_hash[:8]}"
