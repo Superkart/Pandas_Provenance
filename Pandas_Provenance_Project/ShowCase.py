c8df5f927334cfaacbf29abfd728b2509358b88f
Superkart <karthik.ragi15@gmail.com>
2024-12-05 16:12:56 -0600

    Code changes

--- Pandas_Provenance_Project/ShowCase.py
+++ Pandas_Provenance_Project/ShowCase.py
@@ -2,10 +2,9 @@ import pandas as pd
 from provenance.provenance_tracker import ProvenanceTracker
 
 def main():
-    # Initialize the ProvenanceTracker with the relative path to the provenance_log.json
+
     tracker = ProvenanceTracker(log_file="provenance/provenance_log.json")
 
-    # Read the CSV file and track the provenance
     df, table_name = tracker.read_csv("Pandas_Provenance_Project/color_srgb.csv")
     df2 = tracker.read_csv("Pandas_Provenance_Project/industry.csv")
 
@@ -13,17 +12,14 @@ def main():
     print(f"Table Name: {table_name}")
     print(f"DataFrame:\n{df}\n")
 
-    # Example: Filter operation - filter for rows where 'Name' is 'Red'
     filtered_df, filtered_table_name = tracker.filter(df, "Name=='Red'")
     print(f"Filtered Table Name: {filtered_table_name}")
     print(f"Filtered DataFrame:\n{filtered_df}\n")
 
-    # Example: Drop column operation - drop the 'HEX' column
     dropped_df, dropped_table_name = tracker.drop_columns(df, ["HEX"])
     print(f"Dropped Table Name: {dropped_table_name}")
     print(f"Dropped DataFrame:\n{dropped_df}\n")
 
-    # Example: Merge operation - merge the dataframe with itself on 'Name' column
     merged_df, merged_table_name = tracker.merge(df, df, on="Name")
     print(f"Merged Table Name: {merged_table_name}")
     print(f"Merged DataFrame:\n{merged_df}\n")
